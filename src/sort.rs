6e32bb3e1545667f83c4bfbd1b4f498a9ea392a3
Go Cova <gocova.dev@gmail.com>
2024-06-19 18:21:45 -0600

    Using rusty code: Option<T> instead of returning an additional value in tuple.
    Updated:
    ! All the test codes, and documentation

--- src/sort.rs
+++ src/sort.rs
@@ -1,21 +1,17 @@
 use core::cmp::Ordering;
 
 /// Do a [binary search](https://en.wikipedia.org/wiki/Binary_search_algorithm) looking for a
-/// specific `key`, and returning `not_found_value` if not found. ^^
+/// specific `key`, and returning None if not found.
 /// 
 /// # Arguments
 /// 
 /// * `key` - Value to search for
 /// * `data_ptr` - Constant raw pointer to the array. You can get it using 'data_array.as_ptr()'
 /// * `data_length` - Array size. It can be lower than the array capacity.
-/// * `not_found_value` - Value to return if value not found
 /// * `compare_f` - Function used to compare^^
 /// 
 /// # Notes
 /// ^
-/// ^^ This `not_found_value` will limit the possible lenght by -1 element. For example, if
-/// the platform is based on 32bits (like wasm), the recommended value for this is the max
-/// value of u32 (usize): 0xFFFF_FFFF
 /// 
 /// # Example
 /// 
@@ -26,9 +22,8 @@ pub fn bsearch<T1, T2, F>(
     key: T1
     , data_ptr: *const T2
     , data_length: usize
-    , not_found_value: usize
     , compare_f: F
-) -> usize
+) -> Option<usize>
 where F: Fn(&T1, *const T2, usize) -> Ordering, 
 {
     let mut left = 0;
@@ -37,7 +32,7 @@ where F: Fn(&T1, *const T2, usize) -> Ordering,
         let idx = left + (s>>1);
         let r: Ordering = compare_f(&key, data_ptr, idx);
         if r == Ordering::Equal {
-            return idx;
+            return Some(idx);
         }
         if r == Ordering::Greater {
             left = idx + 1;
@@ -45,7 +40,7 @@ where F: Fn(&T1, *const T2, usize) -> Ordering,
         }
         s = s >> 1;
     }
-    return not_found_value;
+    return None;
 }
 
 /// An `AproxBinarySearchResult` is how the `aprox_bsearch` ended:
@@ -156,12 +151,6 @@ where F: Fn(&T1, *const T2, usize) -> Ordering,
     } 
 }
 
-#[derive(Debug)]
-pub enum SortedArrayAllocResult {
-    Ok
-    , ArrayCapacityExceeded
-}
-
 /// For a pre allocated array with capacity C, the sorted_array_insert
 /// will shift the contents to the right if the value doesnt exist in
 /// it, returning the index for the new value. Or only return the index
@@ -173,14 +162,13 @@ pub enum SortedArrayAllocResult {
 /// * `data_ptr` - Constant raw pointer to the array. You can get it using 'data_array.as_ptr()'
 /// * `data_original_length` - Array size. It can be lower than the array capacity
 /// * `data_capacity` - Real allocated array length.
-/// * `not_allocated_value` - Value to return if the capacity was exceeded
 /// * `compare_f` - Function used to compare^^
 /// * `copy_f` - Function to shift elements along the array
 /// 
 /// # Example
 /// ```
 /// use core::cmp::Ordering;
-/// use rselib::sort::{sorted_array_insert, SortedArrayAllocResult};
+/// use rselib::sort::{sorted_array_insert};
 /// 
 /// let mut test_array: [u8; 3] = [0x10, 0x20, 0x00]; // pre allocated array
 /// let new_value: u8 = 0x15;
@@ -191,12 +179,11 @@ pub enum SortedArrayAllocResult {
 /// let mut length = 2; // Used elements in the pre allocated array
 /// 
 /// 
-/// let (res, possible_index, added_count) = sorted_array_insert(
+/// let option_res = sorted_array_insert(
 ///     new_value
 ///     , test_array_ptr
 ///     , length
 ///     , capacity
-///     , not_allocated_value
 ///     , |key, ptr, index| {
 ///         let current_value = unsafe {
 ///             & *(
@@ -224,30 +211,37 @@ pub enum SortedArrayAllocResult {
 ///         *dest = *src;
 ///     }
 /// );
-/// assert!(matches!(res, SortedArrayAllocResult::Ok));
-/// assert_eq!(1, possible_index);
-/// assert_eq!(1, added_count);
 /// 
-/// // Update value
-/// test_array[possible_index] = new_value;
-/// // !Update length
-/// length += added_count;
+/// match option_res {
+///     Some(x) => {
+///         let (possible_index, added_count) = x;
+///         assert_eq!(1, possible_index);
+///         assert_eq!(1, added_count);
 /// 
-/// assert_eq!(0x10, test_array[0]);
-/// assert_eq!(new_value, test_array[1]);
-/// assert_eq!(0x20, test_array[2]);
+///         // Update value
+///         test_array[possible_index] = new_value;
+///         // !Update length
+///         length += added_count;
 /// 
-/// assert_eq!(3, length);
+///         assert_eq!(0x10, test_array[0]);
+///         assert_eq!(new_value, test_array[1]);
+///         assert_eq!(0x20, test_array[2]);
+/// 
+///         assert_eq!(3, length);
+///     }
+///     , None => {
+///         assert!(option_res.is_some());
+///     }
+/// }
 /// ```
 pub fn sorted_array_insert<T1, T2, F1, F2>(
     key: T1
     , data_ptr: *mut T2
     , data_original_length: usize
     , data_capacity: usize
-    , not_allocated_value: usize
     , compare_f: F1
     , copy_f: F2
-) -> (SortedArrayAllocResult, usize, usize)
+) -> Option<(usize, usize)>
 where F1: Fn(&T1, *const T2, usize) -> Ordering, 
 F2: Fn(*mut T2, usize, usize)
 {
@@ -259,7 +253,7 @@ F2: Fn(*mut T2, usize, usize)
     );
     match aprox_result {
         AproxBinarySearchResult::ExactMatchIndex => {
-            return (SortedArrayAllocResult::Ok, possible_index, 0);
+            return Some((possible_index, 0));
         },
         AproxBinarySearchResult::AproxMatch => {
             if data_original_length + 1 <= data_capacity {
@@ -279,18 +273,18 @@ F2: Fn(*mut T2, usize, usize)
                         target_index -= 1;
                     }
                 }
-                return (SortedArrayAllocResult::Ok, possible_index, 1);
+                return Some((possible_index, 1));
             }
             else {
-                return (SortedArrayAllocResult::ArrayCapacityExceeded, not_allocated_value, 0);
+                return None;
             }
         },
         AproxBinarySearchResult::OutsideIndex => {
             if data_original_length + 1 <= data_capacity {
-                return (SortedArrayAllocResult::Ok, possible_index, 1);
+                return Some((possible_index, 1));
             }
             else {
-                return (SortedArrayAllocResult::ArrayCapacityExceeded, not_allocated_value, 0);
+                return None;
             }
         },
     }
@@ -344,34 +338,41 @@ mod tests {
     fn insert_at_the_end() {
         let mut test_array: [u8; 3] = [0x10, 0x20, 0x00]; // pre allocated array
         let new_value: u8 = 0x25;
-        let not_allocated_value: usize = 0xFFFF_FFFF;
 
         let test_array_ptr = test_array.as_ptr() as *mut u8;
         let capacity = test_array.len();
         let mut length = 2; // Used elements in the array
-        let (res, possible_index, added_count) = sorted_array_insert(
+
+        let option_res = sorted_array_insert(
             new_value
             , test_array_ptr
             , length
             , capacity
-            , not_allocated_value
             , u8_cmp
             , u8_cp
         );
 
-        assert!(matches!(res, SortedArrayAllocResult::Ok));
-        assert_eq!(possible_index, 2);
-        assert_eq!(1, added_count);
-
-        // Write to the pointed
-        test_array[possible_index] = new_value;
-        // !!Dont forget to update the used length
-        length += added_count;
-
-        assert_eq!(0x10, test_array[0]);
-        assert_eq!(0x20, test_array[1]);
-        assert_eq!(0x25, test_array[2]);
-        assert_eq!(3, length);
+        match option_res {
+            Some(x) => {
+                let (possible_index, added_count) = x;
+                assert_eq!(possible_index, 2);
+                assert_eq!(1, added_count);
+        
+                // Write to the pointed
+                test_array[possible_index] = new_value;
+                // !!Dont forget to update the used length
+                length += added_count;
+        
+                assert_eq!(0x10, test_array[0]);
+                assert_eq!(0x20, test_array[1]);
+                assert_eq!(0x25, test_array[2]);
+                assert_eq!(3, length);
+        
+            },
+            None => {
+                assert!(option_res.is_some());
+            },
+        }
         
     }
 
@@ -379,88 +380,101 @@ mod tests {
     fn insert_at_the_beginning() {
         let mut test_array: [u8; 3] = [0x10, 0x20, 0x00]; // pre allocated array
         let new_value: u8 = 0x05;
-        let not_allocated_value: usize = 0xFFFF_FFFF;
 
         let test_array_ptr = test_array.as_ptr() as *mut u8;
         let capacity = test_array.len();
         let mut length = 2; // Used elements in the array
-        let (res, possible_index, added_count) = sorted_array_insert(
+        let option_res = sorted_array_insert(
             new_value
             , test_array_ptr
             , length
             , capacity
-            , not_allocated_value
             , u8_cmp
             , u8_cp
         );
 
-        assert!(matches!(res, SortedArrayAllocResult::Ok));
-        assert_eq!(possible_index, 0);
-        assert_eq!(1, added_count);
+        match option_res {
+            Some(x) => {
+                let (possible_index, added_count) = x;
 
-        // Write to the pointed
-        test_array[possible_index] = new_value;
-        // !!Dont forget to update the used length
-        length += added_count;
+                assert_eq!(possible_index, 0);
+                assert_eq!(1, added_count);
 
-        assert_eq!(0x05, test_array[0]);
-        assert_eq!(0x10, test_array[1]);
-        assert_eq!(0x20, test_array[2]);
-        assert_eq!(3, length);
+                // Write to the pointed
+                test_array[possible_index] = new_value;
+                // !!Dont forget to update the used length
+                length += added_count;
+
+                assert_eq!(0x05, test_array[0]);
+                assert_eq!(0x10, test_array[1]);
+                assert_eq!(0x20, test_array[2]);
+                assert_eq!(3, length);
+            }
+            , None => {
+                assert!(option_res.is_some());
+            }
+        }
     }
 
     #[test]
     fn point_to_the_existing_value() {
         let test_array: [u8; 3] = [0x10, 0x20, 0x00]; // pre allocated array
         let new_value: u8 = 0x10;
-        let not_allocated_value: usize = 0xFFFF_FFFF;
 
         let test_array_ptr = test_array.as_ptr() as *mut u8;
         let capacity = test_array.len();
         let length = 2; // Used elements in the array
-        let (res, possible_index, added_count) = sorted_array_insert(
+        let option_res = sorted_array_insert(
             new_value
             , test_array_ptr
             , length
             , capacity
-            , not_allocated_value
             , u8_cmp
             , u8_cp
         );
 
-        assert!(matches!(res, SortedArrayAllocResult::Ok));
-        assert_eq!(possible_index, 0);
+        match option_res {
+            Some(x) => {
+                let (possible_index, added_count) = x;
 
-        assert_eq!(0x10, test_array[0]);
-        assert_eq!(0x20, test_array[1]);
-        assert_eq!(0x00, test_array[2]);
-        assert_eq!(0, added_count);
+                assert_eq!(possible_index, 0);
+
+                assert_eq!(0x10, test_array[0]);
+                assert_eq!(0x20, test_array[1]);
+                assert_eq!(0x00, test_array[2]);
+                assert_eq!(0, added_count);
+            }
+            , None => {
+                assert!(option_res.is_some());
+            }
+        }
     }
 
     #[test]
     fn max_capacity_exceeded() {
         let test_array: [u8; 2] = [0x10, 0x20]; // pre allocated array
         let new_value: u8 = 0x15;
-        let not_allocated_value: usize = 0xFFFF_FFFF;
 
         let test_array_ptr = test_array.as_ptr() as *mut u8;
         let capacity = test_array.len();
         let length = 2; // Used elements in the array
-        let (res, possible_index, added_count) = sorted_array_insert(
+        let option_res = sorted_array_insert(
             new_value
             , test_array_ptr
             , length
             , capacity
-            , not_allocated_value
             , u8_cmp
             , u8_cp
         );
 
-        assert!(matches!(res, SortedArrayAllocResult::ArrayCapacityExceeded));
-        assert_eq!(possible_index, not_allocated_value);
-
-        assert_eq!(0x10, test_array[0]);
-        assert_eq!(0x20, test_array[1]);
-        assert_eq!(0, added_count);
+        match option_res {
+            Some(_) => {
+                assert!(option_res.is_none());
+            }
+            , None => {
+                assert_eq!(0x10, test_array[0]);
+                assert_eq!(0x20, test_array[1]);
+            }
+        }
     }
 }
